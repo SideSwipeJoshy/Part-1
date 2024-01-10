d49969b30778af87fbfb89a787096f4a16bd108d
DESKTOP-APTFB7I\Joshua <hargobij@sheridancollege.ca>
2024-01-10 15:17:14 -0500

    task 2 complete: tank + movement + missle

---
+++ Assets/Week 1/Scripts/Mover.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    float speed = 3f;
+   public GameObject misslePrefab;
+    public Transform spawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direction = Input.GetAxis("Horizontal");
+
+        transform.Translate(direction * speed * Time.deltaTime, 0, 0);
+        if(Input.GetKeyDown(KeyCode.Space)){
+
+            Instantiate(misslePrefab, spawn.position, spawn.rotation);
+           
+
+
+        }
+    }
+}
