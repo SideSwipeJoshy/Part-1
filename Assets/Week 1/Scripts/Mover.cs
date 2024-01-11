134c80d024561c2a1fac1184cef31dea4a9c6c1f
DESKTOP-APTFB7I\Joshua <hargobij@sheridancollege.ca>
2024-01-11 12:45:49 -0500

    correct branch

---
+++ Assets/Week 1/Scripts/Mover.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    float speed = 30f;
+    public GameObject misslePrefab;
+    public Transform spawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direction = Input.GetAxis("Horizontal");
+        transform.Translate(direction * Time.deltaTime, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(misslePrefab, spawn.position, spawn.rotation);
+        }
+
+    }
+
+}
